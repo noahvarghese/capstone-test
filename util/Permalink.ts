0828070564287ffdbe3c261b62e33d9c6f112f1a
noahvarghese <varghese.noah@gmail.com>
2021-04-22 15:25:58 -0400

    optiomized

--- util/Permalink.ts
+++ util/Permalink.ts
@@ -1,13 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+let permalink: string;
+
 interface URL {
     TARGET_DEV?: string;
     TARGET_LOCAL?: string;
     TARGET_PROD?: string;
 }
 
-const getPermalink = () => {
+const setPermalink = () => {
     const {TARGET_DEV, TARGET_ENV, TARGET_LOCAL, TARGET_PROD} = process.env;
 
     const urls: URL[] = [{TARGET_DEV}, {TARGET_LOCAL}, {TARGET_PROD}];
@@ -15,10 +17,17 @@ const getPermalink = () => {
     const urlKey = `TARGET_${TARGET_ENV}` as keyof URL;
     let urlToUse = urls.find((url) => url[urlKey] !== undefined);
 
-    if ( urlToUse ) {
-        return urlToUse[urlKey];
+    if ( urlToUse && urlToUse[urlKey] ) {
+        permalink = urlToUse[urlKey]!;
     }
     throw new Error("Permalink not implmented yet.");
+}
+
+const getPermalink = () => {
+    if ( !permalink ) {
+        setPermalink();
+    }
+    return permalink;
 }
 
 export default getPermalink;
