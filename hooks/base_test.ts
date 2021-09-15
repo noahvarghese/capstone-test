20c0bfef4620b272c8fc3aabd0eded52fb5ec1c2
noahvarghese <varghese.noah@gmail.com>
2021-09-15 17:06:56 +0000

    added headless option

--- hooks/base_test.ts
+++ hooks/base_test.ts
@@ -1,30 +1,40 @@
 import { Before, After } from "@cucumber/cucumber";
 import { Builder, Capabilities, ThenableWebDriver } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome";
 import BaseWorld from "../support/base_world";
 import dotenv from "dotenv";
 dotenv.config();
 
 
-const getDriver = (): ThenableWebDriver => {
+const getDriver = (): ThenableWebDriver | undefined => {
     const capabilities: Capabilities = Capabilities.chrome();
 
-    const browserOptions = {
-        w3c: false,
-    };
+    capabilities.set("chromeOptions", {w3c: false});
 
-    capabilities.set("chromeOptions", browserOptions);
+    if (process.env.TEST_ENV === "CI") {
 
-    const driver = new Builder()
-        .withCapabilities(capabilities)
-        .forBrowser("chrome")
-        .build();
+        const driver = new Builder()
+            .withCapabilities(capabilities)
+            .forBrowser("chrome")
+            .setChromeOptions(new chrome.Options().headless())
+            .build();
 
-    return driver;
+        return driver;
+    }
+
+    else if (process.env.TEST_ENV === "LOCAL") {
+        const driver = new Builder()
+            .withCapabilities(capabilities)
+            .forBrowser("chrome")
+            .build();
+
+        return driver;
+    }
 };
 
 
 Before(async function (this: BaseWorld) {
-        this.setDriver(getDriver());
+        this.setDriver(getDriver()!);
 });
 
 // Set crazy timeouts so the session gets destroyed and the return is received
